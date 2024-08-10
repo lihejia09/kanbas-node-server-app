f0136c8c363f4edd206b44a6cd4909f34d30506a
lihejia09 <jillianli@outlook.com>
2024-08-10 13:46:33 -0700

    new a6 server

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/route.js';
 
+
 const CONNECTION_STRING =
     process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
